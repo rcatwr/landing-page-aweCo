cd681a5f5058b205e61025ef1e296348e727bcee
unknown <richard.minardi@gmail.com>
2021-04-01 13:09:51 -0700

    updated some styles

---
+++ app.js
@@ -0,0 +1,11 @@
+function stopScroll(){
+    console.log('hi!')
+    // const burger = document.querySelector('#hamburger')
+
+    // burger.addEventListener('click', (e) =>{
+    //     console.log(e.target)
+    // })
+
+}
+console.log('test')
+stopScroll();
